c9ea99b1d64a6f907b5d8906b1c9c0826813eb15
Mario Patronelli <mario@raffles.com.ar>
2020-01-09 01:34:23 -0300

    Actualiza InmuebleFilters.php

---
+++ app/Modules/Poga/Filters/InmueblePadreFilters.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Raffles\Modules\Poga\Filters;
+
+use RafflesArgentina\FilterableSortable\BaseFilters;
+
+class InmueblePadreFilters extends BaseFilters
+{
+    /**
+     * direccion.
+     *
+     * @param mixed $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function direccion($query)
+    {
+        $q = explode(',', $query ?: ',,,');
+        return $this->builder->whereHas(
+            'idDireccion', function ($direccion) use ($q) {
+                return $direccion->where('calle_principal', $q[0]);
+        
+                if ($q[1]) {
+                    $direccion->whereBetween('numeracion', [(int($q[1]) - 100), (int($q[1]) + 100)]);
+                } elseif($q[2]) {
+                    $direccon->where('calle_secundaria', $q[2]);
+                } 
+            }
+        );
+    }
+
+    /**
+     * enum_estado
+     *
+     * @param mixed $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function enum_estado($query)
+    {
+        return $this->builder->where('inmuebles_padre.enum_estado', $query);
+    }
+
+    /**
+     * excluir.
+     *
+     * @param mixed $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function excluir($query)
+    {
+        switch ($query) {
+        case 'con_renta':
+            return $this->builder
+                ->with(
+                    ['unidades' => function ($query) {
+                            return $query->whereDoesntHave(
+                                'idInmueble.rentas', function ($query) {
+                                               return $query->where('enum_estado', 'ACTIVO'); 
+                                }
+                            ); 
+                    }]
+                )
+                ->whereDoesntHave(
+                    'idInmueble.rentas', function ($query) {
+                        return $query->where('enum_estado', 'ACTIVO'); 
+                    }
+                );
+        break;
+        case 'sin_renta':
+            return $this->builder
+                ->with(
+                    ['unidades' => function ($query) {
+                            return $query->whereHas(
+                                'idInmueble.rentas', function ($query) {
+                                               return $query->where('enum_estado', 'ACTIVO'); 
+                                }
+                            ); 
+                    }]
+                )
+                ->whereHas(
+                    'idInmueble.rentas', function ($query) {
+                        return $query->where('enum_estado', 'ACTIVO'); 
+                    }
+                );
+        break;
+        }
+    }
+}
