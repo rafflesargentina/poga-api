88996d6111e71d4dd5c18ec6fd6bdbf0e86a2b98
Mario Patronelli <mario@raffles.com.ar>
2020-01-26 21:08:52 -0300

    Modifica el caso de uso ActualizarRenta

--- app/Modules/Poga/UseCases/ActualizarRenta.php
+++ app/Modules/Poga/UseCases/ActualizarRenta.php
@@ -2,60 +2,86 @@
 
 namespace Raffles\Modules\Poga\UseCases;
 
-use Raffles\Modules\Poga\Models\{ Renta, User };
+use Raffles\Modules\Poga\Models\Renta;
 use Raffles\Modules\Poga\Repositories\RentaRepository;
-use Raffles\Modules\Poga\Notifications\RentaActualizada;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use RafflesArgentina\ResourceController\Traits\WorksWithFileUploads;
+use Storage;
 
 class ActualizarRenta
 {
-    use DispatchesJobs;
-    
-    /**
-     * The Renta model.
-     *
-     * @var Renta
-     */
-    protected $renta;
+    use DispatchesJobs, WorksWithFileUploads;
 
     /**
-     * The form data and User model.
+     * The form data and the Renta model.
      *
      * @var array
-     * @var User
      */
-    protected $data, $user;
+    protected $data, $renta;
 
     /**
      * Create a new job instance.
      *
      * @param Renta $renta The Renta model.
      * @param array $data  The form data.
-     * @param User  $user  The User model.
      *
      * @return void
      */
-    public function __construct($renta, $data, $user)
+    public function __construct(Renta $renta, $data)
     {
         $this->renta = $renta;
         $this->data = $data;
-        $this->user = $user;
     }
 
     /**
      * Execute the job.
      *
-     * @param RentaRepository $repository The RentaRepository object.
+     * @param RentaRepository $rRenta The RentaRepository object.
      *
      * @return void
      */
-    public function handle(RentaRepository $repository)
+    public function handle(RentaRepository $rRenta)
     {
-        $renta = $repository->update($this->renta, $this->data)[1];
-    
-        $this->user->notify(new RentaActualizada($renta));
-    
+        $this->sincronizarEstadosInmueble($this->renta);
+
+        $renta = $rRenta->update($this->renta, $this->data)[1];
+
         return $renta;
+    }
+
+    /**
+     * Sincronizar Estados de Inmueble.
+     *
+     * @param Renta $renta The Renta model.
+     *
+     * @return void
+     */
+    protected function sincronizarEstadosInmueble($renta)
+    {
+        if (isset($this->data['estados_inmueble'])) {
+            $estadosInmueble = $this->data['estados_inmueble'];
+            $renta->estados_inmueble()->sync([]);
+    
+            foreach ($estadosInmueble as $estadoInmueble) {
+                $foto = $estadoInmueble['foto'];
+                $uploadedFile = $foto;
+                if ($foto) {
+                    $imageInfo = explode(";base64,", $foto);
+                    if (isset($imageInfo[1])) {
+                        $imageExt = str_replace('data:image/', '', $imageInfo[0]);
+                        $image = str_replace(' ', '+', $imageInfo[1]);
+                        $imageName = "estado-inmueble-".$estadoInmueble['id'].'-'.time().".".$imageExt;
+
+                        $relativePath = $this->getDefaultRelativePath();
+                        $storagePath = $this->getStoragePath($relativePath);
+                        $uploadedFile = $relativePath.$imageName;
+                        Storage::put($uploadedFile, base64_decode($image));
+                    }
+                }
+
+                $renta->estados_inmueble()->syncWithoutDetaching([$estadoInmueble['id'] => ['cantidad' => $estadoInmueble['cantidad'], 'enum_estado' => 'ACTIVO', 'reparar' => $estadoInmueble['reparar'], 'foto' => $uploadedFile]]);
+            }
+        }
     }
 }
