3234f1248fc7bb508233b600136003d7a1d517c1
Mario Patronelli <mario@raffles.com.ar>
2020-02-04 11:14:18 -0300

    Corrige y mejora el caso de uso

--- app/Modules/Poga/UseCases/GenerarMultas.php
+++ app/Modules/Poga/UseCases/GenerarMultas.php
@@ -3,11 +3,10 @@
 namespace Raffles\Modules\Poga\UseCases;
 
 use Raffles\Modules\Poga\Repositories\{ PagareRepository, RentaRepository };
-use Raffles\Modules\Poga\Models\{ Inmueble, Pagare, User };
+use Raffles\Modules\Poga\Models\{ Pagare, User };
 use Raffles\Modules\Poga\Notifications\{ PagareRentaVencidoAcreedor, PagareRentaVencidoDeudor, PagareRentaVencidoParaAdminPoga };
 
 use Carbon\Carbon;
-use GuzzleHttp\Client;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Arr;
 use Illuminate\Queue\SerializesModels;
@@ -28,30 +27,31 @@ class GenerarMultas implements ShouldQueue
      */
     public function handle(RentaRepository $repository, PagareRepository $rPagare)
     {
-	$rentasActivas = $repository->findWhere(['multa' => 1, 'enum_estado' => 'ACTIVO']);
+        $rentasActivasConMulta = $repository->activasConMulta();
 
-        $now = Carbon::now();
+	$now = Carbon::now();
 
-	$pagaresVencidos = collect();
+        $pagaresVencidos = collect();
 
-        foreach($rentasActivas as $renta) {
-	    $fechaInicioRenta = $renta->fecha_inicio;
-
-	    $inmueble = $renta->idInmueble;
-
-	    $inmueble->pagares()
-                ->where('enum_clasificacion_pagare', 'RENTA')
-                ->where('enum_estado', 'PENDIENTE')
-		->where('fecha_vencimiento', '<', $now->toDateString())
-		->get()
-	        ->each(function($item) use($pagaresVencidos) {
-                    $pagaresVencidos->push($item);
-	        });
+        foreach($rentasActivasConMulta as $renta) {
+            $inmueble = $renta->idInmueble;
+            $rPagare->deRentaVencidosParaInmueble($inmueble)
+                ->each(
+                    function ($item) use ($pagaresVencidos) {
+                        $pagaresVencidos->push($item);
+                    }
+                );
 	}
 
 	foreach($pagaresVencidos->unique('id') as $pagareVencido) {
+            // Misma fecha que la del pagare de renta.
+            $fechaPagare = $pagareVencido->fecha_pagare;
+	    // Vence hoy.
+	    $fechaVencimiento = $now;
+
             $renta = $pagareVencido->idRenta;
-            $multaRenta = $renta->multas()->firstOrCreate(
+	    $inmueble = $renta->idInmueble;
+	    $multaRenta = $renta->multas()->firstOrCreate(
                 [ 
                     'id_pagare' => $pagareVencido->id, 
                     'mes' => $now->month, 
@@ -59,44 +59,15 @@ class GenerarMultas implements ShouldQueue
                 ]
 	    );
 
-            $uc = new TraerBoletaPago($pagareVencido->id);
-            $boleta = $uc->handle();
-
-            $inquilino = $pagareVencido->idPersonaDeudora;
-            $targetLabel = $inquilino->nombre_y_apellidos;
-            $targetType = $inquilino->enum_tipo_persona === 'FISICA' ? 'cip' : 'ruc';
-	    $targetNumber = $inquilino->enum_tipo_persona === 'FISICA' ? $inquilino->ci : $inquilino->ruc;
-	    $label = 'Pago de renta para el inquilino '.$targetNumber.' '.$targetLabel.', mes '.Carbon::parse($pagareVencido->fecha_pagare)->format('m/Y');
-            $summary = $label.', con multa por atraso.';
-
-            $inicioMes = $now->copy()->startOfMonth()->toDateString();
-                
-            $pagareMulta = $inmueble->pagares()
-                ->where('enum_estado', 'PENDIENTE')
-		->where('fecha_pagare', '>=', $inicioMes)
-		->where('id_tabla', $renta->id)
-                ->where('enum_clasificacion_pagare', 'MULTA_RENTA')
-		->first();
-
-	    if(!$pagareMulta) {
-                // Notifica solamente la primer vez que genera pagare de multa.
-                $pagareVencido->idPersonaAcreedora->user->notify(new PagareRentaVencidoAcreedor($pagareVencido, $boleta));
-                $pagareVencido->idPersonaDeudora->user->notify(new PagareRentaVencidoDeudor($pagareVencido, $boleta));
-
-                $admin = User::where('email', env('EMAIL_ADMIN_ADDRESS'))->first();
-                if ($admin) {
-                    $admin->notify(new PagareRentaVencidoParaAdminPoga($pagareVencido, $boleta));
-                }
-
-                $fechaCreacionPagare = Carbon::create($now->year, $now->month, $fechaInicioRenta->day, 0, 0, 0);
-		    
-		$monto = $renta->monto_multa_dia;
+	    $pagareMulta = $rPagare->deMultaPendientesDeEsteMesParaRenta($renta)->first();
+	    if (!$pagareMulta) {
+                $monto = $renta->monto_multa_dia;
 
                 $pagareMulta = $rPagare->create(
                     [
                     'id_inmueble' => $inmueble->id,
-		    'fecha_pagare' => $fechaCreacionPagare,
-		    'fecha_vencimiento' => $pagareVencido->idRenta->fecha_fin,
+                    'fecha_pagare' => $fechaPagare,
+                    'fecha_vencimiento' => $fechaVencimiento,
                     'id_persona_acreedora' => $pagareVencido->id_persona_acreedora,
                     'id_persona_deudora' => $pagareVencido->id_persona_deudora,
                     'id_moneda'=> $pagareVencido->id_moneda,
@@ -105,85 +76,91 @@ class GenerarMultas implements ShouldQueue
                     'id_tabla'=> $pagareVencido->id_tabla,
                     'monto' => $monto, 
                     ]
-		 )[1];
+	        )[1];
 
-		if ($boleta) {
-                    $data = $boleta;
+                // Notifica solamente la primer vez que genera pagare de multa.
+                $pagareVencido->idPersonaAcreedora->user->notify(new PagareRentaVencidoAcreedor($pagareVencido));
+                $pagareVencido->idPersonaDeudora->user->notify(new PagareRentaVencidoDeudor($pagareVencido));
 
-		    $itemMulta = [ 
-                        'label' => 'Multa por días de atraso',
-		        'code' => $pagareMulta->id,
-		        'amount' => [
-                            'currency' => 'PYG',
-			    'value' => $monto
-                        ]
-		    ];
-
-                    array_push($data['debt']['description']['items'], $itemMulta);
-		    $data['debt']['amount']['value'] = $boleta['debt']['amount']['value'] + $monto;
-
-		    $debt = ['debt' => Arr::only($data['debt'], ['amount', 'description', 'validPeriod'])];
-
-                    $debt['debt']['description']['summary'] = $summary;
-		    $debt['debt']['description']['text'] = $summary;
-		    $debt['debt']['label'] = $label;
-		    $debt['debt']['validPeriod']['start'] = $data['debt']['validPeriod']['start'];
-		    $debt['debt']['validPeriod']['end'] = $now->toAtomString();
-
-		    $uc = new ActualizarBoletaPago($pagareVencido->id, $debt);
-		    $boleta = $uc->handle();
-		}
+                $admin = User::where('email', env('EMAIL_ADMIN_ADDRESS'))->first();
+                if ($admin) {
+                    $admin->notify(new PagareRentaVencidoParaAdminPoga($pagareVencido));
+                }
 	    } else {
-	        $monto = $pagareMulta->monto + $renta->monto_multa_dia;
-		
-		$pagareMulta->update(
+		$diasAtraso = $fechaVencimiento->diffInDays($pagareVencido->fecha_vencimiento);
+		$monto = $renta->monto_multa_dia * $diasAtraso;
+        
+                $pagareMulta->update(
                     [
+			'fecha_vencimiento' => $fechaVencimiento->toDateString(),    
                         'monto' => $monto, 
                     ]
-                );
-		
-		if ($boleta) {
-		    $data = $boleta;
+		);
+            }
 
-		    $itemExistente = array_search($pagareMulta->id, array_column($boleta['debt']['description']['items'], 'code'));
+            $uc = new TraerBoletaPago($pagareVencido->id);
+	    $boleta = $uc->handle(); 
 
-		    if ($itemExistente) {
-			$data['debt']['amount']['value'] = $boleta['debt']['amount']['value'] + $renta->monto_multa_dia;
-			$data['debt']['description']['items'][$itemExistente]['amount']['value'] = $monto;
-
-			$debt = ['debt' => Arr::only($data['debt'], ['amount', 'description', 'validPeriod'])];
-
-                        $debt['debt']['description']['summary'] = $summary;
-			$debt['debt']['description']['text'] = $summary;
-			$debt['debt']['label'] = $label;
-                        $debt['debt']['validPeriod']['start'] = $data['debt']['validPeriod']['start'];
-			$debt['debt']['validPeriod']['end'] = $now->toAtomString();
-		    } else {
-			$itemMulta = [
-                            'label' => 'Multa por atraso',
-                            'code' => $pagareMulta->id,
-                            'amount' => [
-                                'currency' => 'PYG',
-                                'value' => $monto
-			    ]
-			];
-
-			array_push($data['debt']['description']['items'], $itemMulta);
-			$data['debt']['amount']['value'] = $boleta['debt']['amount']['value'] + $monto;
-
-			$debt = ['debt' => Arr::only($data['debt'], ['amount', 'description', 'validPeriod'])];
-
-                        $debt['debt']['description']['summary'] = $summary;
-			$debt['debt']['description']['text'] = $summary;
-			$debt['debt']['label'] = $summary;
-                        $debt['debt']['validPeriod']['start'] = $data['debt']['validPeriod']['start'];
-			$debt['debt']['validPeriod']['end'] = $now->toAtomString();
-		    }
-
-                    $uc = new ActualizarBoletaPago($pagareVencido->id, $debt);
-                    $uc->handle();
-                }
+	    if ($boleta) {
+                $this->actualizarBoletaPago($boleta, $pagareMulta);
             }
         }
+    }
+
+    /**
+     * Actualizar Boleta de Pago.
+     *
+     * @param array  $boleta
+     * @param Pagare $pagareMulta
+     *
+     * @return void
+     */
+    protected function actualizarBoletaPago(array $boleta, Pagare $pagareMulta)
+    {
+        $data = $boleta;
+
+        $inquilino = $pagareMulta->idPersonaDeudora;
+        $targetLabel = $inquilino->nombre_y_apellidos;
+        $targetNumber = $inquilino->enum_tipo_persona === 'FISICA' ? $inquilino->ci : $inquilino->ruc;
+	$label = 'Pago de renta para el inquilino '.$targetNumber.' '.$targetLabel.', mes '.Carbon::parse($pagareMulta->fecha_pagare)->format('m/Y');
+	$montoMulta = $pagareMulta->monto;
+	$montoRenta = $pagareMulta->idRenta->monto;
+	$summary = $label.', con multa por atraso.';
+	$validPeriodEnd = Carbon::parse($pagareMulta->fecha_vencimiento)->toAtomString();
+	$validPeriodStart = $data['debt']['validPeriod']['start'];
+
+        $itemExistente = array_search($pagareMulta->id, array_column($boleta['debt']['description']['items'], 'code'));
+	if ($itemExistente) {
+            $debt = ['debt' => Arr::only($data['debt'], ['amount', 'description', 'label', 'validPeriod'])];
+            $debt['debt']['amount']['value'] = $montoRenta + $montoMulta;
+            $debt['debt']['description']['items'][$itemExistente]['amount']['value'] = $montoMulta;
+            $debt['debt']['description']['summary'] = $summary;
+            $debt['debt']['description']['text'] = $summary;
+	    $debt['debt']['label'] = $label;
+            $debt['debt']['validPeriod']['end'] = $validPeriodEnd;
+	    $debt['debt']['validPeriod']['start'] = $validPeriodStart;
+        } else {
+            $itemMulta = [
+                'label' => 'Multa por atraso',
+                'code' => $pagareMulta->id,
+                'amount' => [
+                    'currency' => 'PYG',
+                    'value' => $montoMulta
+                ]
+            ];
+
+	    $debt = ['debt' => Arr::only($data['debt'], ['amount', 'description', 'label', 'validPeriod'])];
+            $debt['debt']['amount']['value'] = $montoRenta + $montoMulta;
+            $debt['debt']['description']['summary'] = $summary;
+            $debt['debt']['description']['text'] = $summary;
+	    $debt['debt']['label'] = $summary;
+            $debt['debt']['validPeriod']['end'] = $validPeriodEnd;
+            $debt['debt']['validPeriod']['start'] = $validPeriodStart;
+
+	    array_push($debt['debt']['description']['items'], $itemMulta);
+        }
+
+        $uc = new ActualizarBoletaPago($boleta['debt']['docId'], $debt);
+        $uc->handle();
     }
 }
