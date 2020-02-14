93342b49aaa35495f7ad7a4260446d712048a899
Mario Patronelli <mario@raffles.com.ar>
2020-02-14 12:19:47 -0300

    Actualiza y corrige AnularPagare

--- app/Modules/Poga/UseCases/AnularPagare.php
+++ app/Modules/Poga/UseCases/AnularPagare.php
@@ -5,6 +5,7 @@ namespace Raffles\Modules\Poga\UseCases;
 use Raffles\Modules\Poga\Models\Pagare;
 use Raffles\Modules\Poga\Notifications\EstadoPagareActualizado;
 use Raffles\Modules\Poga\Repositories\PagareRepository;
+use Raffles\Modules\Poga\UseCases\TraerBoletaPago;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 
@@ -40,34 +41,92 @@ class AnularPagare
      */
     public function handle(PagareRepository $repository)
     {
-	$inmueble = $this->pagare->idInmueble;
+        $pagare = $this->pagare;
+        $inmueble = $pagare->idInmueble;
 
-        $data = [
-            "debt" => [
-                "objectStatus" => [
-                    "status" => "canceled"
+	if ($pagare->idRenta->enum_estado === 'FINALIZADO') {
+            $data = [
+                'debt' => [
+                    'objStatus' => [
+                        'status' => 'inactive',
+                    ]
                 ]
-            ]
-        ];
+	    ];
 
-	try {
-	    $boleta = $this->dispatchNow(new ActualizarBoletaPago($this->pagare->id, $data));
-	} catch (\Exception $e) {
-            $boleta = null;
-	}
+            switch ($pagare->enum_clasificacion_pagare) {
+	        case 'RENTA':
+                    $uc = new TraerBoletaPago($pagare->id);	
+		    $boleta = $uc->handle();
+		    $items = $boleta['debt']['description']['items'];
 
-        $pagare = $this->dispatchNow(new ActualizarEstadoPagare($this->pagare, "ANULADO"));
+		    foreach($items as $item) {
+			$p = $repository->find($item['code']);
+                        $this->dispatchNow(new ActualizarEstadoPagare($p, 'ANULADO'));
+		    }
 
-	$administradorReferente = $inmueble->idAdministradorReferente;
-	if ($administradorReferente) {
-	    $personaAdministradorReferente = $administradorReferente->idPersona;
-            if ($personaAdministradorReferente) {
-	        $usuarioAdministradorReferente = $personaAdministradorReferente->user;
-		if ($usuarioAdministradorReferente) {
-                    $usuarioAdministradorReferente->notify(new EstadoPagareActulizado($pagare));
-		}
+                    $pagare = $this->dispatchNow(new ActualizarEstadoPagare($pagare, 'ANULADO'));
+                break;
+		case 'OTRO':
+                    $pagare = $this->dispatchNow(new ActualizarEstadoPagare($pagare, 'ANULADO'));
+		break;
 	    }
+	} elseif ($pagare->idRenta->enum_estado === 'ACTIVO') {
+	    switch ($pagare->enum_clasificacion_pagare) {
+                case 'RENTA':
+                    $data = [
+                        'debt' => [
+                            'objStatus' => [
+                                'status' => 'active',
+                            ]
+                        ]
+                    ];
+
+                    $uc = new TraerBoletaPago($pagare->id);
+                    $boleta = $uc->handle();		
+		    $items = $boleta['debt']['description']['items'];
+
+		    // Arroja el key del item del array.
+		    $multaExistente = array_search('Multa por atraso', array_column($items, 'label'));
+		    if ($multaExistente) {
+			$amountCurrency = $boleta['debt']['amount']['currency'];
+			$amountValue = $boleta['debt']['amount']['value'];
+                        $summary = $boleta['debt']['description']['summary'];
+
+			// Descuenta del total de la boleta el monto de la multa.
+			$data['debt']['amount']['currency'] = $amountCurrency;
+			$data['debt']['amount']['value'] = intVal($amountValue) - intVal($items[$multaExistente]['amount']['value']);
+
+			$data['debt']['description'] = $boleta['debt']['description'];
+
+			// Modifica el summary y text de la boleta para remover ", con multa por atraso."
+			$data['debt']['description']['summary'] = str_replace(', con multa por atraso.', '.', $summary);
+			$data['debt']['description']['text'] = $data['debt']['description']['summary'];
+			
+			// Borra el item de multa.
+			unset($data['debt']['description']['items'][$multaExistente]);
+
+			$p = $repository->find($items[$multaExistente]['code']);
+                        $this->dispatchNow(new ActualizarEstadoPagare($p, 'ANULADO'));
+		    }
+	        break;
+		case 'OTRO':
+                    $data = [
+                        'debt' => [
+                            'objStatus' => [
+                                'status' => 'inactive',
+                            ]
+                        ]
+	            ];
+
+                    $pagare = $this->dispatchNow(new ActualizarEstadoPagare($pagare, 'ANULADO'));
+		break;
+	    }
+        } else {
+            $uc = new TraerBoletaPago($pagare->id);
+            $boleta = $uc->handle();
 	}
+
+	$boleta = $this->dispatchNow(new ActualizarBoletaPago($pagare->id, $data));
 
 	$personaAcreedora = $pagare->idPersonaAcreedora;
 	if ($personaAcreedora) {
